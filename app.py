437bbdf48d7e3f018edee92cec1b771446bf122c
kiooo <101065744+kiookp@users.noreply.github.com>
2023-06-09 12:47:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-
 import time
 import os
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 import telebot
 import undetected_chromedriver as uc
 import configparser
+from selenium.common.exceptions import WebDriverException
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -29,84 +27,96 @@ def send_telegram_message(bot, chat_id, message):
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-software-rasterizer")
+chrome_options.add_argument("--disable-setuid-sandbox")
 driver = uc.Chrome(options=chrome_options)
+
 driver.get(website)
 print("成功打开网站")
 
-wait = WebDriverWait(driver, 10)
-uid_input = wait.until(EC.visibility_of_element_located((By.NAME, "uid")))
+wait = WebDriverWait(driver, 30)  # Increase the wait time to 30 seconds
+
+uid_input = wait.until(EC.presence_of_element_located((By.NAME, "uid")))
 
 uid_input.clear()
 uid_input.send_keys(username)
 
 print("成功输入账号")
 
-password_input = wait.until(EC.visibility_of_element_located((By.ID, "fakePassword")))
+password_input = wait.until(EC.presence_of_element_located((By.ID, "fakePassword")))
 
 password_input.send_keys(password)
 print("成功输入密码")
 
-wait = WebDriverWait(driver, 20)
-login_button = wait.until(
-    EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div[4]/div[3]/div[3]/div[1]/form/div[3]/button")))
+login_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[4]/div[3]/div[3]/div[1]/form/div[3]/button")))
 
-login_button.click()
-
-# 检查是否存在账号或密码错误的提示元素
-error_label_locator = (By.XPATH, "//*[@id='warnOrErrDiv']/label")
-success_element_locator = (By.XPATH, "/html/body/section/article")
-
-
-time.sleep(3)
 try:
-    # 等待登录成功的元素出现
-    wait.until(EC.visibility_of_element_located(success_element_locator))
-    print("登录成功！")
-    bot = telebot.TeleBot(telegram_bot_token)
-    send_telegram_message(bot, telegram_chat_id, f"账号 {username}：登录成功！")
+    login_button.click()
+    # 检查是否存在账号或密码错误的提示元素
+    error_label_locator = (By.XPATH, "//*[@id='warnOrErrDiv']/label")
+    success_element_locator = (By.XPATH, "/html/body/section/article")
 
-    cookies = driver.get_cookies()
-
-    coremail_sid = None
-    coremail = None
-
-    for cookie in cookies:
-        if cookie['name'] == 'Coremail.sid':
-            coremail_sid = cookie['value']
-        elif cookie['name'] == 'Coremail':
-            coremail = cookie['value']
-
-    print("Coremail.sid:", coremail_sid)
-    print("Coremail:", coremail)
-    print("uid:", username)
-    print("开始保活...")
-    send_telegram_message(bot, telegram_chat_id, "开始保活...")
-
-    message = f"Coremail.sid: {coremail_sid}\nCoremail: {coremail}"
-    send_telegram_message(bot, telegram_chat_id, message)
-
-    while True:
-        try:
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#mltree_2_span"))).click()
-            time.sleep(60)
-
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#mltree_3_span"))).click()
-            time.sleep(60)
-
-        except Exception as e:
-            print("出现异常：", str(e))
-            print("正在刷新页面...")
-            driver.refresh()
-
-except:
-    # 如果存在账号或密码错误的提示元素，则表示登录失败
-    if driver.find_elements(*error_label_locator):
-        print("账号或密码错误，登录失败！")
+    try:
+        # 等待登录成功的元素出现
+        wait.until(EC.visibility_of_element_located(success_element_locator))
+        print("登录成功！")
         bot = telebot.TeleBot(telegram_bot_token)
-        send_telegram_message(bot, telegram_chat_id, "账号或密码错误，登录失败！")
-    else:
-        print("登录失败！")
-        bot = telebot.TeleBot(telegram_bot_token)
-        send_telegram_message(bot, telegram_chat_id, "登录失败！")
+        send_telegram_message(bot, telegram_chat_id, f"账号 {username}：登录成功！")
 
-driver.quit()
+        cookies = driver.get_cookies()
+
+        coremail_sid = None
+        coremail = None
+
+        for cookie in cookies:
+            if cookie['name'] == 'Coremail.sid':
+                coremail_sid = cookie['value']
+            elif cookie['name'] == 'Coremail':
+                coremail = cookie['value']
+
+        print("Coremail.sid:", coremail_sid)
+        print("Coremail:", coremail)
+        print("uid:", username)
+        print("开始保活...")
+        send_telegram_message(bot, telegram_chat_id, "开始保活...")
+
+        message = f"Coremail.sid: {coremail_sid}\nCoremail: {coremail}"
+        send_telegram_message(bot, telegram_chat_id, message)
+
+        while True:
+            try:
+                wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#mltree_2_span"))).click()
+                time.sleep(60)
+
+                wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#mltree_3_span"))).click()
+                time.sleep(60)
+
+                wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#mltree_4_span")))
+                driver.refresh()
+                time.sleep(10)
+
+            except Exception as e:
+                print("出现异常：", str(e))
+                print("正在刷新页面...")
+                driver.refresh()
+
+    except:
+        # 如果存在账号或密码错误的提示元素，则表示登录失败
+        if driver.find_elements(*error_label_locator):
+            print("账号或密码错误，登录失败！")
+            bot = telebot.TeleBot(telegram_bot_token)
+            send_telegram_message(bot, telegram_chat_id, "账号或密码错误，登录失败！")
+        else:
+            print("登录失败！")
+            bot = telebot.TeleBot(telegram_bot_token)
+            send_telegram_message(bot, telegram_chat_id, "登录失败！")
+
+except WebDriverException as e:
+    print("登录过程中出现异常:", str(e))
+
+finally:
+    driver.quit()
